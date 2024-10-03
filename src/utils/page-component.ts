aa8d7d93e699dd8ef521d8bb8a127239b38d3996
mehrabix <mehrabi@post.com>
2024-10-03 22:41:34 +0330

    feat: improve performance

--- src/utils/page-component.ts
+++ src/utils/page-component.ts
@@ -9,5 +9,17 @@ export function PageComponent<T extends { new (...args: any[]): LitElement }>(ta
     return this;
   };
 
+  const connectedCallback = target.prototype.connectedCallback;
+  target.prototype.connectedCallback = function () {
+    connectedCallback && connectedCallback.call(this);
+    console.log(`${target.name} component mounted`);
+  };
+
+  const disconnectedCallback = target.prototype.disconnectedCallback;
+  target.prototype.disconnectedCallback = function () {
+    disconnectedCallback && disconnectedCallback.call(this);
+    console.log(`${target.name} component unmounted`);
+  };
+
   return target;
 }
--- src/utils/reusable-component.ts
+++ src/utils/reusable-component.ts
@@ -1,7 +1,19 @@
-import { LitElement } from "lit";
+import { LitElement } from 'lit';
 
-export function ReusableComponent(target: any) {
+export function ReusableComponent<T extends { new (...args: any[]): LitElement }>(target: T) {
   if (!(target.prototype instanceof LitElement)) {
     Object.setPrototypeOf(target.prototype, LitElement.prototype);
   }
+
+  target.prototype.createRenderRoot = function () {
+    return this;
+  };
+
+  const connectedCallback = target.prototype.connectedCallback;
+  target.prototype.connectedCallback = function () {
+    connectedCallback && connectedCallback.call(this);
+    console.log(`Reusable component ${target.name} connected`);
+  };
+
+  return target;
 }
